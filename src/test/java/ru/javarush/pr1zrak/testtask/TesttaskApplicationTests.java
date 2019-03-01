5fdbe574d6388f0bca99aa6c9115e47afc48080c
przrak <przrak@gmail.com>
2019-03-02 00:56:45 +0700

    remove empty method

--- src/test/java/ru/javarush/pr1zrak/testtask/TesttaskApplicationTests.java
+++ src/test/java/ru/javarush/pr1zrak/testtask/TesttaskApplicationTests.java
@@ -1,6 +1,5 @@
 package ru.javarush.pr1zrak.testtask;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
